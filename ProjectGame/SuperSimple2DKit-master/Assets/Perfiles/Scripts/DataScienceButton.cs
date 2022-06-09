3323afadaada4e721b415ce746c849e67bee54f0
Carmina López <a00830265@itesm.mx>
2022-06-09 10:12:45 -0500

    Escena de Perfiles
    Se agregó la escena de perfiles y el botón para que lo mande a su respectiva escena.

---
+++ ProjectGame/SuperSimple2DKit-master/Assets/Perfiles/Scripts/DataScienceButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataScienceButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void DataScience()
+    {
+        SceneManager.LoadScene("DataScience");
+    }
+}
