e20174939e79b1a740ed797b0d92c0589b228903
geraldineecht <geraldineecht@gmail.com>
2022-06-14 18:10:49 -0500

    Script idUsuario

--- ProjectGame/SuperSimple2DKit-master/Assets/Scripts/SampleScene/ScoreAplicante.cs
+++ ProjectGame/SuperSimple2DKit-master/Assets/Scripts/SampleScene/ScoreAplicante.cs
@@ -1,7 +1,23 @@
 [System.Serializable]
 
-public class ScoreAplicante
+// public static class ScoreAplicante
+// {
+//     public int idAplicante;
+//     public int score = 0; 
+// }
+
+public class ScoreAplicante : MonoBehaviour
 {
+    // Start() and Update() methods deleted - we don't need them right now
+
     public int idAplicante;
     public int score = 0; 
+
+    public static ScoreAplicante Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 }
