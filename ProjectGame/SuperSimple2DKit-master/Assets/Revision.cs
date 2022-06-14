0ba3f105fc03902e5b0229c8e67434fe00a9649f
RodrigoGarzaV <90715861+RodrigoGarzaV@users.noreply.github.com>
2022-06-14 03:10:32 -0500

    Revision preguntas

---
+++ ProjectGame/SuperSimple2DKit-master/Assets/Revision.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Revision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public void correctAnswer(){
+        contador.preCorrectas++;
+        contador.numPreguntas++;
+        if(contador.numPreguntas == 20){
+            SceneManager.LoadScene("End");
+        }
+    }
+    public void wrongAnswer(){
+        contador.numPreguntas++;
+        if(contador.numPreguntas == 20){
+            SceneManager.LoadScene("End");
+        }
+    }
+}
